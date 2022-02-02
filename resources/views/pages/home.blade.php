acd7e88fd984aa5a6f58505ab6143bf807630232
BarbonFisso <barbieri.mat@hotmail.it>
2022-02-02 16:48:32 +0100

    Add: login form

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,4 +6,19 @@
         Hello World
     </h1>
 
+     <h2>Login</h2>
+    <form action="{{ route('login') }}" method="POST">
+
+        @method('POST')
+        @csrf
+
+        <label for="email">E-mail</label>
+        <input type="text" name="email"> <br>
+        <label for="password">Password</label>
+        <input type="password" name="password"> <br>
+        <br>
+        <input type="submit" value="LOGIN">
+
+    </form>
+
 @endsection
